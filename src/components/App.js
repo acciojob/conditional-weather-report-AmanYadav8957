5895d034c5e5ada6eec4d64ed0ddc9ecc213b665
AmanYadav8957 <152906478+AmanYadav8957@users.noreply.github.com>
2025-01-31 20:30:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 
 import React from "react";
 import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay"
 
 const App = () => {
+  const [data,setData]=React.useState(null)
   return (
     <div>
         {/* Do not remove the main div */}
